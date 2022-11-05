581075fafa0bf0effb5064de0934c5c203c1376a
wkddntjr1123 <hbo1123@naver.com>
2022-11-05 22:49:41 +0900

    feat : import MongooseModule in CatsModule - for DI

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CatsController } from './cats.controller';
+import { Cat, CatScheme } from './cats.scheme';
 import { CatsService } from './cats.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatScheme }])], // for DI model
   controllers: [CatsController],
   providers: [CatsService],
   // 다른 module에서 캡슐화된 CatsService에 접근 가능. provider로써 DI 가능
