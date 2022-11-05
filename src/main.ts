2d999979157c58af22915cf80166485b1ba5d421
wkddntjr1123 <hbo1123@naver.com>
2022-11-06 01:34:56 +0900

    feat : init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
@@ -7,6 +8,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // for class-validator
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('C.I.C')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); // endpoint : /docs
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL || true,
+    credentials: true,
+  });
   const PORT = process.env.PORT || 8000;
   await app.listen(PORT);
 }
