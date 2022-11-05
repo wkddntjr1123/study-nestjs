c033a51aea99f984245611800d300d414577c9f5
wkddntjr1123 <hbo1123@naver.com>
2022-11-06 05:44:38 +0900

    feat : implement jwt guard / jwt strategy

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,15 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { CatsRepository } from 'src/cats/cats.repository';
+import { Payload } from './jwt.payload';
 
 Injectable();
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    // 음... DI가 undefined되서 확인해보니 순환 참조 때문인듯. https://docs.nestjs.com/fundamentals/circular-dependency
+    @Inject(forwardRef(() => CatsRepository))
+    private readonly catsRepository: CatsRepository,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'sec',
+      secretOrKey: process.env.SECRET_KEY,
       ignoreExpiration: false,
     });
   }
-  // async validate(payload) {}
+
+  async validate(payload: Payload) {
+    const cat = await this.catsRepository.findByIdWithoutPassword(payload.sub);
+    if (cat) {
+      return cat;
+    } else {
+      throw new UnauthorizedException('잘못된 접근입니다.');
+    }
+  }
 }
