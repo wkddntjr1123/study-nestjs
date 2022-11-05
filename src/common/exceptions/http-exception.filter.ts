27de5057f2a331dad5140d2a857c76e954d97cd6
wkddntjr1123 <hbo1123@naver.com>
2022-11-05 20:58:38 +0900

    feat : implement exception filter

---
+++ src/common/exceptions/http-exception.filter.ts
@@ -0,0 +1,35 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+    const error = exception.getResponse();
+
+    if (typeof error === 'string') {
+      response.status(status).json({
+        success: false,
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        message: error,
+        path: request.url,
+      });
+    } else {
+      response.status(status).json({
+        success: false,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        ...error,
+      });
+    }
+  }
+}
