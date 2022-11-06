6cc1ede047c5e0f2bc9f17d9e185ba0e7f4dbe3c
wkddntjr1123 <hbo1123@naver.com>
2022-11-07 01:22:41 +0900

    feat : import MulterModule in CatsModule

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,5 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
 import { AuthModule } from 'src/auth/auth.module';
 import { CatsController } from './cats.controller';
 import { CatsRepository } from './cats.repository';
@@ -9,6 +10,9 @@ import { CatsService } from './cats.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Cat.name, schema: CatScheme }]), // for DI model
+    MulterModule.register({
+      dest: './upload', // for file upload. save in ./upload  상대경로가 현재 프로젝트 디렉토리가 시작
+    }),
     forwardRef(() => AuthModule),
   ],
   controllers: [CatsController],
